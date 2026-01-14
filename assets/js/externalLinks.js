c71229d0fe0476d3349bcaa012c30572b6a98c98
courseappro <courseappro@gmail.com>
2026-01-14 15:43:32 -0300

    Update externalLinks.js

--- assets/js/externalLinks.js
+++ assets/js/externalLinks.js
@@ -1,5 +1,5 @@
-const installerwinUrl = 'https://github.com/josecantero/curseApp/releases/download/v1.1.1/curseApp-1.1.1.AppImage';
-const installerlinUrl = 'https://github.com/josecantero/curseApp/releases/download/v1.1.1/curseApp-Setup-1.1.1.exe';
+const installerwinUrl = 'https://github.com/josecantero/curseApp/releases/download/v1.1.1/curseApp-Setup-1.1.1.exe';
+const installerlinUrl = 'https://github.com/josecantero/curseApp/releases/download/v1.1.1/curseApp-1.1.1.AppImage';
 const grupoFbUrl = 'https://www.facebook.com/groups/cursosgratisenstreaming/';
 
 const btnClass = 'btn primary-cta'
